ae2a24c337ec03e06e54d5a15d07f19bb9acbec8
shradharokade <82148716+shradharokade@users.noreply.github.com>
2021-04-14 19:04:55 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,10 @@
+list1= [12, -7, 5, 64, -14]
+for i in list1:
+    if i >= 0:
+        print(i,end = ' ')
+
+print('\nNow it will implement for second list')
+list2 = [12, 14, -95, 3]
+for value in list2:
+    if value >= 0:
+        print(value,end=' ')
